d70d137a83f82ac2de13a4902a3b83e004c89e9b
Alexander Large <alexanderklarge@gmail.com>
2019-08-14 11:39:56 +0100

    Created a Data Analysis project.py for people to branch and add code to!

---
+++ Data Analysis Project/Data Analysis project.py
@@ -0,0 +1,4 @@
+# todo Get some data (web scrape, load in a .csv, whatever
+# todo Do stuff with the data (visualisation etc)
+
+# Feel free to add comments, ideas, do whatever! Just make your own branch and go crazy, we'll see what happens!
