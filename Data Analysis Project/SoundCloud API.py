de8492ce902f5926a31807d961287898b5951623
Alexander Large <alexanderklarge@gmail.com>
2019-08-14 12:38:04 +0100

    Minor changes

---
+++ Data Analysis Project/SoundCloud API.py
@@ -0,0 +1,8 @@
+# Alex here - just following along with a YouTube tutorial to see what I can do with SoundCloud API
+# Never used APIs before so this should be fun
+
+# Using Python 3.7 right now (as I can't work out how to change to 3.6 in PyCharm)
+# May need to install 3.6
+# (Denis in the Slack stated that 3.6 has more API compatility)
+
+
